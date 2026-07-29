3ef104a0b27d3c4cb5c21504ee0e1fdb77cba4fb
Mykola Stankov <mykola.stankov07@gmail.com>
2026-07-29 21:00:54 +0300

    subrenewal

---
+++ subrenewal/Program.cs
@@ -0,0 +1,22 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration <= 5)
+{
+    discountPercentage = 20;
+    Console.WriteLine($"Your subscription will expire in {daysUntilExpiration} days! Renew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration <= 10)
+{
+    discountPercentage = 10;
+    Console.WriteLine($"Your subscription will expire in {daysUntilExpiration} days! Renew now and save {discountPercentage}%!");
+}
+else
+{
+    Console.WriteLine($"Your subscription will expire in {daysUntilExpiration} days.");
+}
