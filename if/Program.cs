262d3f6722a1c5f421b1df715d831a8095fba4d4
Mykola Stankov <mykola.stankov07@gmail.com>
2026-07-29 20:40:05 +0300

    presents added

--- if/Program.cs
+++ if/Program.cs
@@ -10,22 +10,33 @@ Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+
+    Console.WriteLine($"Your total including the bonus: {total}");
 }
 
-if ((roll1 == roll2) && (roll2 == roll3)) 
+if (total >= 16)
 {
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
+    Console.WriteLine("You win a new car!");
 }
-
-if (total >= 15)
+else if (total >= 10)
 {
-    Console.WriteLine("You win!");
+    Console.WriteLine("You win a new laptop!");
 }
-
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
 else
 {
-    Console.WriteLine("You lose!");
+    Console.WriteLine("Sorry, you lose.");
 }
--- strings/StringFormatting/obj/Debug/net8.0/StringFormatting.AssemblyInfo.cs
+++ strings/StringFormatting/obj/Debug/net8.0/StringFormatting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StringFormatting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b54f73b96d1b5ab366f44461c49bd6531791c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20b81e025d583a644323cf238dc7a2f34e64ee06")]
 [assembly: System.Reflection.AssemblyProductAttribute("StringFormatting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StringFormatting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
