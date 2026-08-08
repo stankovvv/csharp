2bc5f883554fc03fbf8d3a89466b385dcad99481
stankovvv <mykola.stankov07@gmail.com>
2026-08-08 14:05:27 +0300

    formatting

--- blocksAndVarScopes/Program.cs
+++ blocksAndVarScopes/Program.cs
@@ -1,15 +1,25 @@
-﻿bool flag = true;
+﻿/* bool flag = true;
 
 int value=0;
 if (flag)
-{
+
     
     Console.WriteLine($"Inside the block: {value}");
-}
+
 value = 10;
-Console.WriteLine($"Outside the block: {value}");
+Console.WriteLine($"Outside the block: {value}"); */
 //ACCESSING THE VARIABLE OUTSIDE THE BLOCK WILL CAUSE AN ERROR
 //Console.WriteLine($"Outside the block: {value}"); 
 //error too
 /* int value = 20;
 Console.WriteLine($"Outside the block: {value}"); */
+string name = "Steve";
+
+if (name == "Bob") 
+    Console.WriteLine("Found Bob");
+    
+else if (name == "Steve")
+     Console.WriteLine("Found Steve");
+
+else 
+    Console.WriteLine("Found Chuck");
