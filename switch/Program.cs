54fd76da3ed8c0244c825d3bfe9fbc8f50560e55
stankovvv <mykola.stankov07@gmail.com>
2026-08-09 16:20:52 +0300

    works

---
+++ switch/Program.cs
@@ -0,0 +1,27 @@
+﻿// when you change the value of employeeLevel to 201, it will go to default case and print Associate as title.
+int employeeLevel = 201;
+
+
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+    case 100:
+    
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
+        break;
+}
+Console.WriteLine($"Employee: {employeeName}, Title: {title}");
