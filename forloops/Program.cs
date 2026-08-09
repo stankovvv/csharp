3e0624c8e5c77741a9d9247a67a722d2049d476d
stankovvv <mykola.stankov07@gmail.com>
2026-08-09 16:47:16 +0300

    fizzBuzz

---
+++ forloops/Program.cs
@@ -0,0 +1,19 @@
+﻿/* //adds 3
+
+for (int i = 0; i < 10; i += 3)
+{
+    Console.WriteLine(i);
+} */
+
+string[] names = {"Alex", "Eddie", "David", "Michael" };
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    if (names[i] == "David")
+    {
+        names[i] = "Sunny";
+    }
+}
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
