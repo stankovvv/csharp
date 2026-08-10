fdb32ba97db77057c8c39a35fb6110c443f0680b
stankovvv <mykola.stankov07@gmail.com>
2026-08-10 13:31:24 +0300

    roleChecker

---
+++ AdminOrUser/Program.cs
@@ -0,0 +1,24 @@
+﻿string? readRes;
+string roleName= "";
+
+bool validRole = false; 
+do
+{
+    Console.WriteLine("Enter your role (Admin, manager, user):");
+    readRes = Console.ReadLine();
+    if (readRes != null)
+    {
+        roleName = readRes.Trim();
+    }
+    if (roleName.ToLower() == "administrator" || roleName.ToLower() == "manager" || roleName.ToLower() == "user")
+    {
+        validRole = true;
+    }
+    else
+    {
+        Console.Write($"The role name that you entered, \"{roleName}\" is not valid. ");
+    }
+} while (validRole == false);
+
+Console.WriteLine($"Your role name ({roleName}) has been accepted.");
+readRes = Console.ReadLine();
